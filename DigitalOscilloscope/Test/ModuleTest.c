be2bc2ee051352762e4d44093f6f18fe5fa30458
David <david19@github.com>
2020-05-02 22:36:39 +0200

    1. Module Test environment added 2. SysTick Timing functions successfully modultested

---
+++ DigitalOscilloscope/Test/ModuleTest.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "Led.h"
+#include "Timing.h"
+
+#if MODULE_TEST_ON //Content is uncommented in order to no complain for 2 main at switching
+				//--> TODO: at switch, deleting .elf or other Debug files shall also solve this issue.
+
+void ModuleTest_SysTickFunctions();
+int32_t ModuleTest_SysTickTickStopPart(int32_t _lastTick, int32_t _startingTick);
+
+int main(int argc, char* argv[])
+{
+  // At this stage the system clock should have already been configured
+  // at high speed.
+	ModuleTest_SysTickFunctions();
+	//TODO: use Green and Red LED if test succeeded or failed.
+	return 0;
+
+}
+
+int32_t ModuleTest_SysTickTickStopPart(int32_t _lastTick, int32_t _startingTick)
+{
+	int32_t lastTick, elapsedTick, startingTick;
+
+	lastTick = _lastTick;
+	startingTick = _startingTick;
+
+	elapsedTick = (int32_t)(startingTick)-lastTick; //TODO: test if substraction of to uint32_t how results int32_t
+
+	//Handle negative output (in case of down-counting reached 0 during tick measurement)
+	if(elapsedTick < 0)
+	{
+		elapsedTick = SysTick->LOAD + elapsedTick; //Add the load value when SysTick turned 0->LOAD at the end of downcounting
+	}
+
+	return elapsedTick;
+}
+
+void ModuleTest_SysTickFunctions()
+{
+	uint8_t isSysTickEnabled = 0;
+	uint32_t elapsedTime[7];
+	double elapsedTimeInSec[3];
+	int32_t SysTickTickStopPart[7];
+
+	Led_Init();
+	SysTick_Enable();
+	isSysTickEnabled = SysTick_IsEnabled();
+	elapsedTime[0] = SysTick_GetElapsedTimeSinceStartInMs();
+	elapsedTimeInSec[0] = SysTick_GetElapsedTimeSinceStartInSec();
+
+	SysTick_DelayInMs(1); //prevent that SysTick_TimerStart() saves 0ms (stop takes it as not started)
+	SysTick_TimerStart();
+	SysTick_TickStart();
+	Led_Toggle();
+	elapsedTime[1] = SysTick_TickStop();
+	elapsedTime[2] = SysTick_TickStop();
+	SysTick_DelayInMs(300);
+	elapsedTime[3] = SysTick_TimerStop();
+	elapsedTime[4] = SysTick_TimerStop();
+	elapsedTime[5] = SysTick_GetElapsedTimeSinceStartInMs();
+	elapsedTimeInSec[1] = SysTick_GetElapsedTimeSinceStartInSec();
+
+	//manual test of SysTick in SysTick_TickStop()
+	SysTickTickStopPart[0] = ModuleTest_SysTickTickStopPart(1,0);
+	SysTickTickStopPart[1] = ModuleTest_SysTickTickStopPart(0,1);
+	SysTickTickStopPart[2] = ModuleTest_SysTickTickStopPart(1,1);
+	SysTickTickStopPart[3] = ModuleTest_SysTickTickStopPart(1,SysTick->LOAD);
+	SysTickTickStopPart[4] = ModuleTest_SysTickTickStopPart(1,SysTick->LOAD-1000);
+	SysTickTickStopPart[5] = ModuleTest_SysTickTickStopPart(SysTick->LOAD,1);
+	SysTickTickStopPart[6] = ModuleTest_SysTickTickStopPart(SysTick->LOAD-1000,1);
+
+	while (1)
+	{
+	 Led_Toggle();
+	 SysTick_DelayInMs(300);
+	 elapsedTimeInSec[2] = SysTick_GetElapsedTimeSinceStartInSec();
+	 elapsedTime[6] = SysTick_GetElapsedTimeSinceStartInMs();
+	}
+}
+#endif
