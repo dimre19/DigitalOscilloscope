d63e453859ad1f581a7d059ab6b093dd678377fc
David Imre <david19@github.com>
2020-05-13 07:09:31 +0200

    Updated I2CHandler for reading internal temperature received on I2C

--- DigitalOscilloscopeClient/I2cHandler.py
+++ DigitalOscilloscopeClient/I2cHandler.py
@@ -1,35 +1,11 @@
-# i2ctest.py
-# A brief demonstration of the Raspberry Pi I2C interface, using the Sparkfun
-# Pi Wedge breakout board and a SparkFun MCP4725 breakout board:
-# https://www.sparkfun.com/products/8736
-'''
+# I2cHandler.py
+# Handle I2C Communication
+
 import smbus
+import time
 
 # I2C channel 1 is connected to the GPIO pins
 channel = 1
-
-#  STM32F4 address
-address = 0x45
-reg_write_dac = 0x40 #Why??
-
-# Initialize I2C (SMBus)
-bus = smbus.SMBus(channel)
-msg = [0xAB]
-# Write out I2C command: address, reg_write_dac, msg[0], msg[1]
-bus.write_i2c_block_data(address, reg_write_dac, msg)
-
-print("I2C done")
-'''
-# i2ctest.py
-# A brief demonstration of the Raspberry Pi I2C interface, using the Sparkfun
-# Pi Wedge breakout board and a SparkFun MCP4725 breakout board:
-# https://www.sparkfun.com/products/8736
-
-import smbus
-
-# I2C channel 1 is connected to the GPIO pins
-channel = 1
-
 #  MCP4725 defaults to address 0x60
 address = 0x7F
 
@@ -44,5 +20,8 @@ msg = [0xAB]
 
 # Write out I2C command: address, reg_write_dac, msg[0], msg[1]
 #bus.write_i2c_block_data(address, reg_write_dac, msg)
-bus.read_i2c_block_data(address,0x40,1)
+for i in range(15):
+    tempValue = bus.read_i2c_block_data(address,0x40,1)
+    time.sleep(1) 
+    print(tempValue)
 
