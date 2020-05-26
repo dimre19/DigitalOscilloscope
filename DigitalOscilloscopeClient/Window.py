005e3384f89c47a14308bf49e28b8eb6d8776de1
David Imre <dimre19@github.com>
2020-05-26 22:13:47 +0200

    Basic matplotlib: 10 element real time changing graph

--- DigitalOscilloscopeClient/Window.py
+++ DigitalOscilloscopeClient/Window.py
@@ -7,29 +7,73 @@
 
 #root.mainloop()
 
-import matplotlib, numpy, sys
-matplotlib.use('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-if sys.version_info[0] < 3:
-    import Tkinter as Tk
-else:
-    import tkinter as Tk
+#import matplotlib, numpy, sys
+#matplotlib.use('TkAgg')
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#from matplotlib.figure import Figure
+#if sys.version_info[0] < 3:
+#    import Tkinter as Tk
+#else:
+#    import tkinter as Tk
+#
+#root = Tk.Tk()
+#
+#f = Figure(figsize=(5,4), dpi=100)
+#ax = f.add_subplot(111)
+#
+#data = (20, 35, 30, 35, 27)
+#
+#ind = numpy.arange(5)  # the x locations for the groups
+#width = .5
+#
+#rects1 = ax.bar(ind, data, width)
+#
+#canvas = FigureCanvasTkAgg(f, master=root)
+#canvas.show()
+#canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+#
+#Tk.mainloop()
 
-root = Tk.Tk()
 
-f = Figure(figsize=(5,4), dpi=100)
-ax = f.add_subplot(111)
+#import matplotlib.pyplot as plt
+#
+#plt.axis([0, 10, 0, 1])
+#
+#for i in range(10):
+#    y = i
+#    plt.scatter(i, y)
+#    plt.pause(1)
+#
+#plt.show()
 
-data = (20, 35, 30, 35, 27)
+import random
+from itertools import count
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-ind = numpy.arange(5)  # the x locations for the groups
-width = .5
+plt.style.use('fivethirtyeight')
 
-rects1 = ax.bar(ind, data, width)
+x_values = []
+y_values = []
 
-canvas = FigureCanvasTkAgg(f, master=root)
-canvas.show()
-canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+index = count()
 
-Tk.mainloop()
+
+def animate(i):
+    if i < 10:
+        x_values.append(next(index))
+        y_values.append(random.randint(0, 5))
+        
+    else:        
+        y_values[i % 10] = (random.randint(0, 5))
+        
+        
+    plt.cla()
+    plt.plot(x_values, y_values)
+
+
+ani = FuncAnimation(plt.gcf(), animate, 1000)
+
+
+plt.tight_layout()
+plt.show()
